50407d0a2a65f4f80189dff2a94ae7cdb5f9e65d
applandsys <applandsys@gmail.com>
2022-11-24 18:52:47 +0600

    COntent entry korte hobe urgently

--- pages/admin/article/content/_id/add_text.vue
+++ pages/admin/article/content/_id/add_text.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="content_area">
       <div style="display: flex; justify-content: space-between">
-        <div><h2> Article Text ={{content_id}}</h2></div>
+        <div><h2> Article Text </h2></div>
         <div>
             <nuxt-link class="btn btn-primary" :to="`/admin/article/${content_id}/add_content`">  Add Content </nuxt-link>
         </div>
@@ -13,11 +13,9 @@
       <div class="row">
         <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
             <div class="">
-
               <form @submit.prevent="formSubmit">
-
                   <div class="form-group">
-                    <label for="content"> Text Content </label>
+                    <label for="content"> Text Content  </label>
                     <editor
                         api-key="73t8fjsg0wa37r22h1gnoznhivgiwf2sxzvt50fu3jdigjo0"
                         :init="{
@@ -40,21 +38,21 @@
                   </div>
                   <div class=" text-center"> <button type="submit" class="btn btn-primary text-center">Submit</button></div>
                 </form>
-
             </div>
         </div>
 
-        <div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
-            <div class="">
-                <h2>Subtitle </h2>
-                <p>Text hobe</p>
-            </div>
-        </div>
+          <div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+              <div class="">
+                  <h2>Tex content</h2>
+                  <p>{{text_content}}</p>
+              </div>
+          </div>
 
       </div>
     </div>
   </div>
 </template>
+
 <script>
  import Editor from '@tinymce/tinymce-vue';
 
@@ -69,36 +67,38 @@ export default {
       token : this.$auth.strategy.token.get(),
       content_id: this.$route.params.id,
       form_data:{
-        content_id: this.$route.params.id,
-        content_subtitle: null,
-        content_type:null,
-        layout:null,
-        layout_width:null,
+        article_id:1,
+        article_content_id:1,
         content:null,
+        font:'san-serif',
+        font_size:'22',
       },
-      article_content: {},
-
+      text_content: {},
     }
   },
   methods:{
+
     async formSubmit(){
-      let res =  await this.$axios.post('api/v1/auth/article-content/store',this.form_data,{ headers: {
+      let res =  await this.$axios.post('api/v1/auth/text-content/store',this.form_data,{ headers: {
         'Authorization': `Basic ${this.token}`
       }});
     },
 
-    async getArticleContent(){
-      let res =  await this.$axios.get(`api/v1/auth/article-content/show/${this.content_id}`,
+    async getTextContent(){
+      let res =  await this.$axios.get(`api/v1/auth/text-content/get/${this.content_id}`,
                                           { headers: {
-                                                        'Authorization': `Basic ${this.token}`
+                                                        'Authorization': `Basic ${this.token}`,
+                                                        'Accept': `Application/json`,
                                                       }
                                           }
                                      );
-          this.article_content =  res.data;
+          this.text_content =  res.data;
     }
+
+
   },
   mounted(){
-    this.getArticleContent();
+    this.getTextContent();
   }
 
 }
